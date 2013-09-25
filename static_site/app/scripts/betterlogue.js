098bf58c751f118d7c888d8049d18bdada6446bb
Daniel Alan Miller <dalan.miller@gmail.com>
2013-09-25 11:41:36 +0930

    Working demo

--- static_site/app/scripts/betterlogue.js
+++ static_site/app/scripts/betterlogue.js
@@ -1,15 +1,50 @@
-function BetterlogueCtrl($scope) {
+function BetterlogueCtrl($scope, $http) {
   
-  //$scope.classList = $.get('class_list.json', function(doc){return JSON.parse(doc)});
+  	$scope.test = ["1","2","3"];
+  	
+  	// $http({method: 'GET', url: '/someUrl'}).
+	  // success(function(data, status, headers, config) {
+	  //   // this callback will be called asynchronously
+	  //   // when the response is available
+	  // }).
+	  // error(function(data, status, headers, config) {
+	  //   // called asynchronously if an error occurs
+	  //   // or server returns response with an error status.
+  	// });
 
+ 	console.log("Attempting to get JSON and parse the classes file");
+ 	$http({method: 'GET', url: 'scripts/classes.json'}).
+ 		success(function(d){
+			d.forEach(function(t){
+
+				t.keywords = [t.title, t.professor, t.subject, t.num].join(" ");
+				t.visible = "none";
+			});
+
+			$scope.classes = d;
+
+  		});
+
+  	
+
+
+ 
   $scope.filter = function() {
+
+
+
     //$scope.todos.push({text:$scope.todoText, done:false});
     //$scope.todoText = '';
   };
 
   $scope.update = function() {
-    console.log("wtf")
     console.log($scope.query)
+
+
+  }
+
+  $scope.debugClasses = function(){
+  	console.log(classes);
   }
 
 }
